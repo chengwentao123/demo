13bc481366cf9f64d9a3265dc6b0204b886b3bcc
chengwentao123 <1173421676@qq.com>
2019-03-17 11:47:14 +0800

    test

---
+++ src/main/java/demo123/Test.java
@@ -0,0 +1,16 @@
+package demo123;
+
+/**
+ * Created by hc on 2019-03-17.
+ */
+public class Test {
+   /* public void main(String[]args){
+        system.out.println("pingguo");
+    }
+*/
+    public static void main(String[] args) {
+       System.out.println("only one");
+
+
+        }
+        }
